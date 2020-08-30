234611913961f726f41bd046516ebc9feb83adaa
jhlee <jhlee@neofect.com>
2020-08-30 16:13:57 +0900

    yarn lint 완료

--- src/service/loginAPI.js
+++ src/service/loginAPI.js
@@ -1,20 +1,19 @@
 import axios from 'axios'
-import { LoaderOptionsPlugin } from 'webpack'
 
 const getUserInfo = (uid, password) => {
 	return axios.get('/endpoint-for-get-user-info', {
 		params: {
-			'uid': uid,
-			'password': password
-		}
+			uid: uid,
+			password: password,
+		},
 	})
 }
 
 const isFinished = uid => {
 	return axios.get('/endpoint-for-is-finished', {
 		params: {
-			'uid': uid
-		}
+			uid: uid,
+		},
 	})
 }
 
@@ -23,14 +22,16 @@ export default {
 		try {
 			const getUserInfoPromise = await getUserInfo(uid, password)
 			const isFinishedPromise = await isFinished(uid)
-			const [userInfoResponse, isFinishedResponse] = await Promise.all([getUserInfoPromise, isFinishedPromise])
-			
+			const [userInfoResponse, isFinishedResponse] = await Promise.all([
+				getUserInfoPromise,
+				isFinishedPromise,
+			])
+
 			if (userInfoResponse.data.length === 0) return 'noAuth'
-			if (isFinishedResponse.data[0]).CNT > 0) return 'done'
+			if (isFinishedResponse.data[0].CNT > 0) return 'done'
 			return userInfoResponse
-		}
-		catch( err ) {
+		} catch (err) {
 			console.error(err)
 		}
-	}
+	},
 }
